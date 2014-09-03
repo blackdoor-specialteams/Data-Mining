d1371c004075c73b375e677ab649b4decb2d0a1a
Cj Buresch <cjburesch@gmail.com>
2014-09-03 16:54:18 -0700

    stub file and datasets

---
+++ HW1/hw1.py
@@ -0,0 +1,15 @@
+"""
+@Names: Cj Buresch and Nathan Fischer
+@Date: 9/3/2014
+@ Homework #1 -- Dataset Manipulation
+@Description: 
+	Reads datasets from file, prints the number of instances from each. 
+	Combine the two datasets into one and print the result. Resolves any
+	missing attribute data for instances accordingly. LAstly, computes 
+	summary data from the combined datasets. 
+@Version: Python v2.7
+"""
+
+def main():
+
+main()
