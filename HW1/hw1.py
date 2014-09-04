90b414e253c3f96affc9df8045f4fe1ad4e6b20b
Cj Buresch <cjburesch@gmail.com>
2014-09-03 17:53:15 -0700

    more stub work

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -9,7 +9,44 @@
 	summary data from the combined datasets. 
 @Version: Python v2.7
 """
+import csv
+
+def readDataset(filename):
+	file = open(filename,"rb")
+	reader = csv.reader(file)
+
+	return []
+
+def countInstances():
+	return 0
+	
+def checkForDuplicates():
+	return []
+	
+def printDatasetInfo():
+
+def combineTwoDatasets(data1,data2):
+
+
+def stepTwo(filename):
+	dataset = readDataset(filename)
+	
+	print("--------------------------------------------------")
+	print(filename)
+	print("--------------------------------------------------")
+	
+	duplicates = checkForDuplicates(dataset)
+	instcount = countInstances(dataset)
+	
+	print("No. of instances: ", instcount)
+	print("Duplicates: ", duplicates )
+	
+def stepThree():
+
+def stepFour():
 
 def main():
-
+	stepTwo("auto-mpg.txt")
+	
+	raw_input("Hit Enter to EXIT")
 main()
