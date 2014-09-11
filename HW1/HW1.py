793216c9ad7bc1658a8c212af9b1cf438ce5c94b
Cj Buresch <cjburesch@gmail.com>
2014-09-11 14:08:29 -0700

    comments and things

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -13,7 +13,6 @@ import csv
 import os
 from tabulate import tabulate
 
-
 def get_dataset(file_name):
 	""" Reads a csv file and returns a table as a list of lists"""
 	the_file = open(file_name)
@@ -24,9 +23,8 @@ def get_dataset(file_name):
 			table.append(row)
 	return table
 	
-#returns a list of duplicates	
-#keys are attributes to be considered part of the key
 def check_for_duplicates(file_name, keys = ()):
+	"""" Generates a list of duplicates from a csv using specified keys"""
 	dupes = []
 	instances = []
 	with open(file_name,"r") as input_file:
@@ -52,6 +50,7 @@ def clean_data(filename_in,filename_out):
 				writer.writerow(row)
 	
 def count_instances(file_name):
+	"""Counts the number of rows or instances in a csv file"""
 	with open(file_name, "r") as f:
 		return sum(1 for _ in f)
 
@@ -66,8 +65,35 @@ def delete_dups_from_csv(filename_in,filename_out):
 				writer.writerow(row)
 				unique_rows.append(row)
 
-def print_summary_stats(file_name):
+def populate_empty_values(filein):
+	""" Finds null values in a csv file and replaces them with the average for that attribute"""
+	attribute_list = [0,1,3,4,5,7,9]
 
+	for x in attribute_list:
+		datalist = get_att_list(filein,x)
+		avg = round(average(datalist),2)
+		replace_all_NA_for_att(filein,x,avg)
+
+def replace_all_NA_for_att(filein,index,avg):
+	"""Reads the file in, iterates through every row and replaces NA in relevant row index with 
+	the average."""
+	writerows = []
+	
+	with open(filein, 'rb') as _in:
+		f1 = csv.reader(_in)
+		writerows.append(f1.next())
+		for row in f1:
+			if row[index] == 'NA':
+				row[index] = avg
+			writerows.append(row)
+
+	with open(filein, "wb") as out:
+		writer = csv.writer(out)
+		for x in writerows:
+			writer.writerow(x)
+
+def print_summary_stats(file_name):
+	""" Prints a 5 number summary of each attribute using tabulate"""
 	print "Summary Stats:"
 
 	headers = ["attribute","min","max","mid","avg","med"]
@@ -77,6 +103,7 @@ def print_summary_stats(file_name):
 	print tabulate(table,headers,tablefmt="rst")
 
 def make_table(filein,summarylist):
+	""" Builds a table from a csv file, with a set of attributes to include"""
 	table = []
 	for x in summarylist:
 		att_list = get_att_list(filein,x)
@@ -86,13 +113,8 @@ def make_table(filein,summarylist):
 		#print_att_summary_row(get_attribute_name(file_name,x),statlist)
 	return table
 
-def print_att_summary_row(attname,stat_list):
-	print str(attname),
-	for x in stat_list:
-		print str(round(x,2)),
-	print 
-
 def get_att_list(filein,index):
+	""" Collects all attribute data from a csv file for a single attribute"""
 	att_list = []
 	with open(filein, 'rb') as _in:
 		f1 = csv.reader(_in)
@@ -103,12 +125,14 @@ def get_att_list(filein,index):
 	return att_list
 
 def get_attribute_name(filein,index):
+	""" gets label for an attribute at a certain index"""
 	with open(filein, 'rb') as _in:
 		f1 = csv.reader(_in)
 		line1 = f1.next()
 	return line1[index]
 
 def summerize_data(attlist):
+	""" Returns a list of the 5 number summary based on the provided list"""
 	statlist = []
 
 	lmin = min(attlist)
@@ -117,15 +141,16 @@ def summerize_data(attlist):
 	lavg = average(attlist)
 	lmed = median(attlist)
 
-	statlist.append(lmin)
-	statlist.append(lmax)
-	statlist.append(lmid)
-	statlist.append(lavg)
-	statlist.append(lmed)
+	statlist.append(round(lmin,2))
+	statlist.append(round(lmax,2))
+	statlist.append(round(lmid,2))
+	statlist.append(round(lavg,2))
+	statlist.append(round(lmed,2))
 
 	return statlist
 
 def median(alist):
+	""" Returns the median of a list"""
         srtd = sorted(alist) 
         mid = len(alist)/2   
         if (len(alist) % 2) == 0:  
@@ -133,11 +158,13 @@ def median(alist):
         else:
                 return srtd[mid]
 
-def average(numbers):  
-        return float(sum(numbers))/len(numbers)
+def average(numbers): 
+	""" Finds the average for a list"""
+	return float(sum(numbers))/len(numbers)
 
-def midpoint(a,b):  
-        return (a + b) / 2.0
+def midpoint(a,b):
+	""" Finds the midpoint for a list"""
+	return (a + b) / 2.0
 
 def get_key_from_attribs(instance, attribs = ('model_year', 'car_name')):
 	key = ''
@@ -182,8 +209,6 @@ def resolve_price_but_no_mpg_cases(file_name, output):
 				else:
 					write_csv_line(wf, inst, keyset)
 
-
-
 def join_into_file( l_file, r_file, out, keys = ('model_year', 'car_name')):
 	temp_file = 'soManyDups.tmp'
 	l_keyset = []
@@ -222,9 +247,6 @@ def join_into_file( l_file, r_file, out, keys = ('model_year', 'car_name')):
 	delete_dups_from_csv(temp_file,out)
 	os.remove(temp_file)
 
-def combine_two_datasets(data1,data2):
-	return None
-
 def step_two(filename):
 	#dataset = read_dataset(filename)
 	
@@ -239,9 +261,12 @@ def step_two(filename):
 	print "Duplicates: " + str(duplicates)
 	
 def step_three(lfile,rfile,outfile):
+	""" Joins two files into one, using a full outer join"""
 	join_into_file( lfile, rfile, outfile)
 
 def step_four(filein,fileout):
+	""" Cleans data of intances that are missing mpg information. Prints new data
+		information"""
 	clean_data(filein,fileout)
 
 	print "--------------------------------------------------"
@@ -256,7 +281,13 @@ def step_four(filein,fileout):
 	print "Duplicates: " + str(duplicates)
 	
 def step_five(filein):
+	"""prints 5 number summaries for all relevant attributes"""
 	print_summary_stats(filein)
+
+def step_six(filein):
+	""" Finds null values in each relevant attribute and replaces it with the 
+		average for that attribute"""
+	populate_empty_values(filein)
 
 def main():
 
@@ -269,7 +300,8 @@ def main():
 
 	step_five("auto-data-cleaned.txt")
 
-	raw_input("Hit Enter to EXIT")
+	step_six("auto-data-cleaned.txt")
 
+	raw_input("Hit Enter to EXIT")
 
 main()
