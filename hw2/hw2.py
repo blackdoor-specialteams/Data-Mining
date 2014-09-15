f7989bd640a7c8069a2f0d26e2c6a38c6de028a5
Cj Buresch <cjburesch@gmail.com>
2014-09-15 12:21:20 -0700

    adding hw2

---
+++ hw2/hw2.py
@@ -0,0 +1,14 @@
+"""
+@Names: Cj Buresch and Nathan Fischer
+@Date: 9/15/2014
+@ Homework #2 -- Data Visualization
+@Description: 
+
+@Version: Python v2.7
+"""
+import matplotlib.pyplot as pyplot
+
+def main():
+	
+	
+main()
