5efe8ff49ef6b5371761567b47da17b607bd8989
Cj Buresch <cjburesch@gmail.com>
2014-09-15 15:29:56 -0700

    work work work

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -13,21 +13,32 @@ import matplotlib.pyplot as pyplot
 def create_freq_diagram(filein,attlist):
 	"""Create a Frequency diagram for each catagorical attribute in the prepared dataset"""
 	pyplot.figure()
-	#define x and y values
-	xs = []
-	ys = []
-	#calculate a range (make y bigger)
-	xrng = numpy.arange(len(xs))
-	yrng = numpy.arange(0,max(ys)+ 100,50)
-	#create the bar chart
-	pyplot.bar(xrng,ys,.45,align ='center')
-	#define x and y ranges (and value labels)
-	pyplot.xticks(xrng,['foo','bar','baz','quz'])
-	pylot.yticks(yrng)
-	# turn on the backround grid
-	pyplot.grid(True)
-	# save the result to a pdf file
-	pyplot.savefig('fig1.pdf')
+	
+	att_names = get_att_names(filein)
+
+	for att in attlist:
+		#set graph title form attribute names
+		pyplot.title(att_names[att])
+		#define x and y values
+		xs = []
+		ys = []
+
+		#get a list of all of the values from a column
+		#get the count of frequienes from that list, for each value
+		xs, ys = get_frequencies(get_all_att_values(filein,att))
+
+		#calculate a range (make y bigger)
+		xrng = numpy.arange(len(xs))
+		yrng = numpy.arange(0,max(ys)+ 100,50)
+		#create the bar chart
+		pyplot.bar(xs,ys,.45,align ='center')
+		#define x and y ranges (and value labels)
+		pyplot.xticks(xrng,['foo','bar','baz','quz'])
+		pylot.yticks(yrng)
+		# turn on the backround grid
+		pyplot.grid(True)
+		# save the result to a pdf file
+		pyplot.savefig('fig1.pdf')
 
 def create_pie_chart(filein,attlist):
 
@@ -41,7 +52,11 @@ def calculate_linear_regressions(filein,attlist):
 
 def scatter_plot_with_regression(filein,attlist):
 
-def get_get_attName():
+def get_att_names(filein):
+	with open(filein, 'rb') as _in:
+		f1 = csv.reader(_in)
+		names = f1.next()
+		return names
 
 def get_frequencies(xs):
 	ys = sorted(xs)
@@ -53,6 +68,17 @@ def get_frequencies(xs):
 		else:
 			count[-1] += 1
 		return values, counts
+
+def get_all_att_values(filein,index):
+	""" Collects all attribute data from a csv file for a single attribute"""
+	att_list = []
+	with open(filein, 'rb') as _in:
+		f1 = csv.reader(_in)
+		next(f1)
+		for row in f1:
+			if row[index] != 'NA':
+				att_list.append(float(row[index]))
+	return att_list
 
 def main():
 	catagorical_att_list = [2,3,8]
