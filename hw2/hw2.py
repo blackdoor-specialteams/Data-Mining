5b23fbcafc23836103070a7abfe8466273d094b9
Cj Buresch <cjburesch@gmail.com>
2014-09-15 12:36:28 -0700

    yeah yeah

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -3,12 +3,26 @@
 @Date: 9/15/2014
 @ Homework #2 -- Data Visualization
 @Description: 
-
+	
 @Version: Python v2.7
 """
 import matplotlib.pyplot as pyplot
 
+def create_freq_diagram(filein):
+
+def create_pie_chart(filein):
+
+def create_a_dot_plot(filein):
+
+def create_histogram(filein):
+
+def create_scatter_plot(filein):
+
+def calculate_linear_regressions(filein):
+
+def scatter_plot_with_regression(filein):
+
 def main():
-	
-	
+
+
 main()
