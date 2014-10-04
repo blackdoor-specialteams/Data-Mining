c84a94cb2b7f47230a651fc3ce8aa0ee82ee7949
Cj Buresch <cjburesch@gmail.com>
2014-10-04 14:50:03 -0700

    titanic and stuff

--- hw3/cj.py
+++ hw3/cj.py
@@ -1,4 +1,8 @@
 import math
+import csv
+import re
+import os
+from tabulate import tabulate
 
 def my_function():
 	print "This now works"
--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,5 +1,10 @@
 from cj import *
 
+import csv
+import re
+import os
+from tabulate import tabulate
+
 def main():
 	my_function()
 
