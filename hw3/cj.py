571ea3d0eb3770c7cedd953636eeb9b228a348ce
Cj Buresch <cjburesch@gmail.com>
2014-10-13 19:28:35 -0700

    step 3 working! go team! go go go go

--- hw3/cj.py
+++ hw3/cj.py
@@ -6,7 +6,10 @@ import re
 import random
 import os
 import heapq
+import numpy
+from operator import itemgetter
 from tabulate import tabulate
+from decimal import Decimal 
 
 #CJ: 3,4,5
 
@@ -26,53 +29,155 @@ corresponding mpg ranking, and then show their actual mpg ranking:
 
 
 def step3(table,atts):
-	nb_v1(table,atts)
-	return None
-
-def nb_v1(table,):
-	training,test = holdout_partition(table)
-	rules = learning_nb_v1(training)
-
-	for row in test:
-		print_instance(row)
-		out = " "
-
-def learning_nb_v1(tset):
+	keycol = 1
 	checkatts = [2,3,4]
-	dict_of_classes = {}
-	for row in tset:
-		if str(get_mpg_rating(row[1])) not in 
-
+	nb_v1(table,checkatts,keycol)
+	nb_v2(table,checkatts,keycol)
 	return None
 
-def init_dict(classlist,dictlist):
-	for x in classlist:
+def nb_v1(table,attlist,keycol):
+	print "\n" + "Naive Bayes v1"
+	nbtable = temp_table_with_NHTSA_rating(table)
+	training,test = holdout_partition(nbtable)
+	rules = build_all_class_dicts(training,keycol,attlist)
+	rand_inst = get_random_indexes(test,5,len(test))
 
-def get
+	for row in rand_inst:
+		print_instance(row)
+		print_classification(row,rules,keycol,attlist)
 
-def get_NHTSA_rating(x):
+def print_classification(row,rules,clscol,attlist):
+	out = "prediction: " + str(classify(row,attlist,rules)) + ", "
+	out += "actual: " + str(row[clscol])
+	print out
+
+def classify(inst,attlist,rules):
+	prob = []
+	for k in rules.keys():
+		p = rules[k].get(-1)
+		attkeys = rules[k].copy()
+		#print attkeys
+		attkeys.pop(-1,None) 
+		#f
+		for j in attkeys.keys():
+		#for every attribute in this class's dict
+			if inst[j] in rules[k][j].keys():
+			#if the value is in the key set
+				p = float(p) * float(rules[k][j].get(inst[j]))
+			else:
+				p = float(0.0)
+				break
+		prob.append((p,k))
+	#sort by prob, then return the first elem, which is the key
+	prob.sort(key=lambda x: float(x[0]), reverse=True)
+	#print prob
+	return prob[0][1]
+
+#////////////////////////////////////////////////////////////////
+#BUILD TRAINING DATA STRUCT
+#////////////////////////////////////////////////////////////////
+
+def build_all_class_dicts(table,keycol,attlist):
+	""" builds a dict of all classes in the dataset, based on the keycol"""
+	keydict,total = get_class_keys_and_counts(table,keycol)
+	train_dict = {}
+	for k in keydict.keys():
+		train_dict.update({k:build_class_dict(table,keycol,k,attlist,keydict.get(k),total)})
+	return train_dict
+
+def build_class_dict(table,keycol,clskey,attlist,clscount,total):
+	"""builds a dict on a single class for the attlist of the table
+	Also appends the probablity of this class as -1"""
+	cls = {}
+	for x in attlist:
+		cls.update({x:build_att_dict_for_class(table,keycol,clskey,x,clscount)})
+	#add the probablit of the class
+	cls.update({-1: (float(clscount) / float(total))})
+	return cls
+
+
+def build_att_dict_for_class(table,keycol,clskey,col,clscount):
+	"""Builds the dict of a single attribute for a class on the column"""
+	att_d = {}
+	#Build dict with counts for each key
+	for row in table:
+		if row[keycol] == clskey:
+			if row[col] not in att_d.keys():
+				att_d.update({row[col]:1})
+			else:
+				att_d[row[col]] = att_d.get(row[col]) + 1
+	#get probablity for each key of the attribute
+	clsp = {}
+	for k in att_d.keys():
+		p = (float(att_d.get(k)) / float(clscount))
+		clsp.update({k:p})
+
+	return clsp
+
+def get_class_keys_and_counts(table,col):
+	"""Finds the count for each class in the table by column, and returns totaa count"""
+	cls = {}
+	count = 0
+	for row in table:
+		count += 1
+		if  row[col] not in cls:
+			cls.update({row[col]:1})
+		else:
+			cls[row[col]] = cls.get(row[col]) + 1
+	return cls, count
+
+#////////////////////////////////////////////////////////////////
+
+def rebuild_table_with_mpg_rating(table):
+	for row in table:
+		row[1] = get_mpg_rating(row[1])
+
+def temp_table_with_NHTSA_rating(table):
+	tmp = table
+	for row in tmp:
+		row[4] = get_NHTSA_rating(row[4])
+	return tmp
+
+def get_NHTSA_rating(y):
+	x = float(y)
 	if x >= 3500:
-		return 5
+		return '5'
 	elif x > 3000:
-		return 4
+		return '4'
 	elif x > 2500:
-		return 3
+		return '3'
 	elif x > 2000:
-		return 2 
+		return '2' 
 	else:
-		return 1
-
-def classify(inst,rules):
-	return None
+		return '1'
 
 #///////////////////////////////////////////////////////
-def nb_v2():
-	return None
+def nb_v2(table,attlist,keycol):
+	print "\n" + "Naive Bayes v2"
+	nbtable = build_table_with_gaussian(table,4)
+	training,test = holdout_partition(nbtable)
+	rules = build_all_class_dicts(training,keycol,attlist)
+	rand_inst = get_random_indexes(test,5,len(test))
+	for row in rand_inst:
+		print_instance(row)
+		print_classification(row,rules,keycol,attlist)
+
+def build_table_with_gaussian(table,col):
+	tmp = table
+	glist = []
+	for row in tmp:
+		glist.append(float(row[col]))
+	arr = numpy.array(glist)
+	mean = numpy.mean(arr,axis=0)
+	std = numpy.std(arr,axis=0)
+	for row in tmp:
+		row[col] = str(guassian(float(row[col]),mean,std))
+	return tmp
 
 def guassian(x,mean,sdev):
 	first, second = 0,0
 	if sdev > 0:
-		first = 1 / (math.sprt(2*math.pi) * sdev)
+		first = 1 / (math.sqrt(2*math.pi) * sdev)
 		second = math.e ** (-((x - mean) ** 2) / (2 *(sdev ** 2)))
 	return first * second
 
@@ -80,7 +185,7 @@ def holdout_partition(table):
 	randomized = table [:]
 	n = len(table)
 	for i in range(n):
-		j = randint(0,n-1)
+		j = random.randint(0,n-1)
 		randomized[i], randomized[j] = randomized[j],randomized[i]
 	n0 = (n*2)/3
 	return randomized[0:n0],randomized[n0:]
@@ -152,40 +257,45 @@ def table_from_csv(filename):
 
 def print_instance(row):
 	out = "instance: "
-	for x in row:
-		out += " " + str(x)
+	for x in range(len(row) - 1):
+		out += str(row[x]) + ", "
+	out += str(row[len(row) - 1])
 	print out
 
-def get_random_indexes(list,n, size):
+def get_random_indexes(table,n, size):
 	index = sorted([int(random.random()*(size-1)) for _ in range(0, n)])
-	for x in index:
-		
+	result = []
+	for i in index:
+		result.append(table[i])
+	return result
 
-def get_mpg_rating(x):
+def get_mpg_rating(y):
+	x = float(y)
 	if x >= 45:
-		return 10
+		return '10'
 	elif x >= 37:
-		return 9
+		return '9'
 	elif x >= 31:
-		return 8
+		return '8'
 	elif x >= 27:
-		return 7
+		return '7'
 	elif x >= 24:
-		return 6 
+		return '6' 
 	elif x >= 20:
-		return 5
+		return '5'
 	elif x >= 17:
-		return 4
+		return '4'
 	elif x >= 15:
-		return 3 
+		return '3' 
 	elif x > 13:
-		return 2
+		return '2'
 	elif x <= 13:
-		return 1
+		return '1'
 
 def main():
 	atts,table = table_from_csv("auto-data-cleaned.txt")
-	step3(table,atts[2:4])
+	rebuild_table_with_mpg_rating(table)
+	step3(table,atts)
 	#step4()
 	#step5()
 
