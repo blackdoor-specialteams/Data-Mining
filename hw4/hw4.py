6867dfacbed8c6e6107dab9d7e484c619655c6f7
Cj Buresch <cjburesch@gmail.com>
2014-10-19 17:08:17 -0700

    stuff is now added, playah

---
+++ hw4/hw4.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+import csv
+import re
+#import random
+#import math
+import TDIDT
+import lib
+from tabulate import tabulate
+
+
+'''
+################################################################################
+################################################################################
+People's Republic of North Lead By Glorious Party Leader Nate
+DMZ 214
+Southern Provience of Code, True Heir to the Throne: CJ
+################################################################################
+################################################################################
+'''
+
+#////////////////////////////////////////////////////
+def step1(filename):
+	print '===========================================\nSTEP 1: TDIDT -- Titanic \n==========================================='
+	ddict = dataset_from_file(filename)
+	kfold = lib.k_folds(ddict,10)
+	tdidt_cm = lib.init_nxn(2)
+
+	for f in kfold:
+		training,test = lib.holdout_partition(f)
+		#s1_tree_run(tdidt_cm,training,test)
+
+	#s6_print_confusion_table("TDIDT", nb)
+
+def s1_tree_run(cm,traning,test):
+	key = "survived"
+	attlist = ["class","age","sex","survived"]
+	tree = build_tree_from_dataset(traning,attlist)
+	pred = []
+	actual = []
+	for row in test:
+		actual.append(row[key]) 
+		#pred.append(tree.classify(row,attlist,rules))
+	s1_update_cm(cm,actual,pred)
+
+def s1_update_cm(cm,a,p):
+	for i in range(len(a)):
+		cm[YN_to_int(a[i])][YN_to_int(p[i])] += 1
+
+def YN_to_int(i):
+	if i == "yes":
+		return 1
+	else:
+		return 0
+def int_to_YN(i):
+	if i == 1:
+		return "yes"
+	else:
+		return "no"
+
+def step2(filename):
+	return None
+
+def step3(filename):
+	return None
+
+def step4(filename):
+	return None
+
+#//////////////////////////////////////////////////
+
+def build_tree_from_dataset(dset,attributes):
+	t = TDIDT.TDIDT('ATT', attributes[0])
+	for row in dset:
+		t.put_row(row,attributes)
+	return t
+
+def dataset_from_file(filename):
+	"""Returns a  list of dictionaries from the file"""
+	result = []
+	with open(filename, 'r') as f:
+		reader = csv.DictReader(f)
+		for row in reader:
+			result.append(row)
+	return result
+
+def main():
+	dataset = "titanic.txt"
+	step1(dataset)
+	
+if __name__ == '__main__':
+    main()
