a6efc65c4557ff59dc5cdd41168e032438ae78bd
Cj Buresch <cjburesch@gmail.com>
2014-10-31 21:42:17 -0700

    Adding hw5 stub! Get Stoked!!

---
+++ hw5/hw5.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import csv
+import re
+#import random
+import math
+import TDIDT
+import lib
+import operator
+from tabulate import tabulate
+
+
+'''
+################################################################################
+################################################################################
+People's Republic of North Lead By Glorious Party Leader Nate
+DMZ 214
+Southern Provience of Code, True Heir to the Throne: CJ
+################################################################################
+################################################################################
+'''
+def step1():
+	return None
+def step2():
+	return None
+
+
+def main():
+	dataset1 = "titanic.txt"
+	dataset2 = "auto-data-cleaned.txt"
+	
+if __name__ == '__main__':
+    main()
