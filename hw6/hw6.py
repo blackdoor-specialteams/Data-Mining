5f4fe39e23604cb2e18f9e508b1cf5e5ab6e78e8
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-12-12 11:17:30 -0800

    worked on hw6 need to make sets into dicts need to associate supported sets with rules

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -2,7 +2,100 @@
 
 import math
 import lib
+import copy
 
+SUPPORT_DICT = dict()
+
+def get_support(itemset, dataset):
+	count = 0
+	support = 0
+	if(itemset in SUPPORT_DICT):
+		support = SUPPORT_DICT[itemset]
+	else:
+		for entry in dataset:
+			all_in = True
+			entry_list = entry.values()
+			for item in itemset:
+				if item not in entry_list:
+					all_in = False
+					break
+			if all_in:
+				count += 1
+		support = count/float(len(dataset))
+		SUPPORT_DICT[itemset] = support
+	return support
+
+def get_confidence(itemset):
+	return None
+
+def get_completeness(itemset):
+	return None
+
+def itemsets_equal(a, b):
+	if len(a) != len(b):
+		return False
+	for e in a:
+		if e not in b:
+			return False
+	return True
+
+def itemsets_union(a, b):
+	u = copy.deepcopy(a)
+	for item in b:
+		if item not in u:
+			u.append(item)
+	return u
+
+def get_L1(dataset, minsup = .5):
+	l1 = []
+	for entry in dataset:
+		for e in entry.values():
+			if e not in l1 and get_support([e], dataset) >= minsup:
+				l1.append([e])
+	return l1
+
+def num_common_items(a, b):
+	count = 0
+	for e in a:
+		if e in b:
+			count += 1
+	return count
+
+def set_in_setofsets(_set, setofsets):
+	for __set in setofsets:
+		if itemsets_equal(__set, _set):
+			return True
+	return False
+
+def get_Lk(last_l, dataset, minsup = .5):
+	candidates = get_candidate_set(last_l)
+	Lk = []
+	for candidate in candidates:
+		if get_support(candidate, dataset) >= minsup:
+			Lk.append(candidate)
+	return Lk
+
+def get_candidate_set(itemset):
+	candidates = []
+	for a in last_l:
+		for b in last_l:
+			if not itemsets_equal(a,b):
+				if(num_common_items(a, b) = len(a) - 1):
+					union = itemsets_union(a,b)
+					if(set_in_setofsets(union, candidates)):
+						candidates.append(union)
+	return candidates
+
+def hw6():
+	file_name = "titanic.txt"
+	rules = []
+	dataset = lib.dataset_from_file(file_name)
+
+	l = get_L1(dataset)
+	while l:
+		rules += l
+		l = get_Lk(l, dataset)
+	
 
 '''
 ################################################################################
