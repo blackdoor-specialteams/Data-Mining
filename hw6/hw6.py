32f5ad827640e2df8e33b2666cdf7c23c9c35286
Nathanael Fischer <nfischer3@zagmail.gonzaga.edu>
2014-12-12 15:07:47 -0800

    finding supported item sets complete. mushrooms make me feel bad about my complexity

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -5,24 +5,36 @@ import lib
 import copy
 
 SUPPORT_DICT = dict()
+MINSUP = .8
+
+def hash_dict(d):
+	h = ""
+	keys = sorted(d.keys())
+	for key in keys:
+		h += str(key)
+		h += str(d[key])
+	return h
 
 def get_support(itemset, dataset):
+	global SUPPORT_DICT
 	count = 0
 	support = 0
-	#if(itemset in SUPPORT_DICT):
-	#	support = SUPPORT_DICT[itemset]
-	#else:
-	for entry in dataset:
-		all_in = True
-		for item_key in itemset.keys():
-			if itemset[item_key] != entry[item_key]:
-				all_in = False
-				break
-		if all_in:
-			count += 1
-	support = count/float(len(dataset))
-	#SUPPORT_DICT[itemset] = support
+	h = hash_dict(itemset)
+	if(h in SUPPORT_DICT):
+		support = SUPPORT_DICT[h]
+	else:
+		for entry in dataset:
+			if subset(itemset, entry):
+				count += 1
+		support = count/float(len(dataset))
+		SUPPORT_DICT[h] = support
 	return support
+
+def subset(subset, superset):
+	for item_key in subset.keys():
+		if subset[item_key] != superset[item_key]:
+			return False
+	return True
 
 def get_confidence(itemset):
 	return None
@@ -46,13 +58,17 @@ def itemsets_union(a, b):
 			u[key] = b[key]
 	return u
 
-def get_L1(dataset, minsup = .5):
+def get_L1(dataset, minsup = MINSUP):
 	l1 = []
 	for entry in dataset:
 		for key in entry.keys():
 			e = {key:entry[key]} # e is an itemset
-			if e not in l1 and get_support(e, dataset) >= minsup:
-				l1.append(e)
+			if e not in l1: 
+				sup = get_support(e, dataset)
+				#print e
+				#print sup
+				if(sup) >= minsup:
+					l1.append(e)
 	return l1
 
 def num_common_items(a, b):
@@ -68,36 +84,53 @@ def set_in_setofsets(_set, setofsets):
 			return True
 	return False
 
-def get_Lk(last_l, dataset, minsup = .5):
+def get_Lk(last_l, dataset, minsup = MINSUP):
 	candidates = get_candidate_set(last_l)
 	Lk = []
 	for candidate in candidates:
-		if get_support(candidate, dataset) >= minsup:
+		subsets = get_subsets(candidate)
+		supported = True
+		for subset in subsets:
+			if not set_in_setofsets(subset, last_l):
+				supported = False
+				break
+		if supported:#if get_support(candidate, dataset) >= minsup:
 			Lk.append(candidate)
 	return Lk
 
+def get_subsets(itemset):
+	subsets = []
+	for key in itemset:
+		subset = copy.copy(itemset)
+		subset.pop(key)
+		subsets.append(subset)
+	#print itemset
+	#print subsets
+	return subsets
+
 def get_candidate_set(itemset):
 	candidates = []
-	for a in itemset:
-		for b in itemset:
-			if a != b:
-				if(num_common_items(a, b) == len(a) - 1):
-					union = itemsets_union(a,b)
-					if(set_in_setofsets(union, candidates)):
+	for a in range(0,len(itemset)):
+		for b in range(a,len(itemset)):
+			if itemset[a] != itemset[b]:
+				if(num_common_items(itemset[a], itemset[b]) == (len(itemset[a]) - 1)):
+					union = itemsets_union(itemset[a],itemset[b])
+					if(not set_in_setofsets(union, candidates)):
 						candidates.append(union)
+	#print candidates
 	return candidates
 
 def hw6():
-	file_name = "titanic.txt"
+	file_name = "agaricus-lepiota.txt"
 	supp_sets = []
 	dataset = lib.dataset_from_file(file_name)
 
 	l = get_L1(dataset)
 	while l:
-		print l
 		supp_sets += l
 		l = get_Lk(l, dataset)
-	print supp_sets
+	for itemset in supp_sets:
+		print itemset
 	
 
 '''
